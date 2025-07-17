b637ca5b29ebcdd26e26af1a3201c757451a1635
Yuzuki <yuzuki.s.371@akane.waseda.jp>
2025-07-17 18:40:56 +0900

    Disable database initialization on asset precompile

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,8 @@ module Norikobrand
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
 
+    config.assets.initialize_on_precompile = false
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
